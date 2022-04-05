bc37cd167421dd81440158411dd3122c28430e92
ElyasCe <you@example.com>
2022-04-05 23:20:41 +0200

    resolve conflit

--- themes/heticwp_marmiton/functions.php
+++ themes/heticwp_marmiton/functions.php
@@ -29,13 +29,40 @@ function marmishlag_add_roles()
         'moderate_comments' => true
     ]);
 }
-
-
-
 add_action('init', 'marmishlag_add_roles');
+
+
+add_action('send_headers','site_router');
+function site_router(){
+  $root = str_replace('index.php','',$_SERVER['SCRIPT_NAME']);
+  $url = str_replace($root,'',$_SERVER['REQUEST_URI']);
+  $url = explode('/',$url);
+  if(count($url) == 1 && $url[0] =='login'){
+    require "tpl-login.php";
+    die();
+  }else
+if(count($url) == 1 && $url[0] =='profil'){
+    require "tpl-profil.php";
+    die();
+  }
+  if(count($url) == 1 && $url[0] =='logout'){
+    wp_logout();
+    $redirect_url = home_url();
+         wp_safe_redirect( $redirect_url );
+         exit;
+    
+  }
+    if(count($url) == 1 && $url[0] =='register'){
+     require "tpl-register.php";
+    die();
+  }
+}
+
+add_filter('show_user_bar','__return_true');
+add_action('init', ' marmishlag_add_roles');
 add_action('after_setup_theme', "marmishlag_theme_support");
 add_action('after_switch_theme', function () {
     flush_rewrite_rules();
 });
-// add_action('add_meta_boxes', 'marmishlag_add_custom_box');
+
 // add_action('wp_enqueue_script', 'marmishlag_register_assets');
