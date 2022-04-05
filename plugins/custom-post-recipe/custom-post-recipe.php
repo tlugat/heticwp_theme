d0f04e7ca030eaa881e6b3003e150a742fe3f850
tlugat <teo.lugat@hetic.net>
2022-04-05 18:33:10 +0200

    feat: custom post type with metabox refactoring

--- plugins/custom-post-recipe/custom-post-recipe.php
+++ plugins/custom-post-recipe/custom-post-recipe.php
@@ -2,31 +2,8 @@
 /*
 Plugin Name: Recipe plugin
 */
-require_once(__DIR__ . '/includes/Terms.php');
+require_once(__DIR__ . '/includes/Custom-terms.php');
+require_once(__DIR__ . '/includes/Custom-post.php');
 
-$terms = new Terms();
-
-function register_custom_post_type()
-{
-    register_post_type(
-        'recipes',
-        array(
-            'labels' => array(
-                'name' => 'Recette',
-                'singular_name' => 'Recette',
-                'search_items' => 'Rechercher une recette',
-                'all_items' => 'Toutes les recettes'
-            ),
-            'public' => true,
-            'show_in_rest' => true,
-            'supports' => array('title', 'editor', 'thumbnail'),
-            'has_archive' => true,
-            'rewrite'   => array('slug' => 'recettes'),
-            'menu_position' => 5,
-            'menu_icon' => 'dashicons-food',
-            'taxonomies' => array('variety') // this is IMPORTANT
-        )
-    );
-}
-
-add_action('init', 'register_custom_post_type');
+$terms = new Custom_Terms();
+$recipe_custom_post = new Custom_Post();
